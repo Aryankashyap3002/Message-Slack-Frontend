7c4994b09237020bb3e5c94737ee4c46ed796d9a
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-03-18 12:43:20 +0530

    Basic signup card for authorisation

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import { SignupCard } from '@/components/organisms/Auth/SingupCard' 
+ 
+ export const Auth = () => {
+     return (
+         <div 
+             className="h-[100vh] flex items-center justify-center bg-[#5c3B58]"
+         >
+             <div className="md:h-auto md:w-[420px]">
+                 <SignupCard />
+             </div>
+ 
+         </div>
+     );
+ };
